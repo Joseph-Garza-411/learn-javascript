c6dc8c6eeb41bacea997fa43bce7015e84b1fa92
Joseph Garza <teacherjosephg@gmail.com>
2021-11-05 13:03:27 -0600

    Add comments.

--- learn-javascript_/05_nesting/exercises/A_nested_loops_exercise/snippet-2.js
+++ learn-javascript_/05_nesting/exercises/A_nested_loops_exercise/snippet-2.js
@@ -1,9 +1,12 @@
 //snippet 
 
-for(let n = 0; n < 2; n++){
-    console.log("n=" + n); 
-for(let m = 0; m < 5; m++){
-    console.log("  m=" + m); 
-    }
-    console.log("n=" +n); 
-}
+// for(let n = 0; n < 2; n++){
+//     console.log("n=" + n); 
+// for(let m = 0; m < 5; m++){
+//     console.log("  m=" + m); 
+//     }
+//     console.log("n=" +n); 
+// }
+var myStr =  "This is the first sentence. "; 
+myStr += " \n \n \t This is the second sentence. "; 
+console.log(myStr);   
